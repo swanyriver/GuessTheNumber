9a7a16de4dda40d23e015913a1caff12c6cb3a66
Brandon Swanson <github@brandonswanson.com>
2014-06-25 22:55:34 -0700

    added play loop, check for win/loose

--- numberGuess.cpp
+++ numberGuess.cpp
@@ -21,8 +21,10 @@ using namespace std;
 int main(){
 
     //CONSTANT CONTROL VARIABLES
-	const int MAX_GUESSES = 6;
+	const int MAX_GUESSES = 2;
     const int MAX_RANGE = 2*2*2*2*2*2;
+    
+    const int LINE_WIDTH = 80;
     
     //DATA VARIABLES
     int secretNumber;
@@ -34,33 +36,63 @@ int main(){
     int rangeBegining = 1;
     int rangeEnd = MAX_RANGE;
     
+    string playAgain;
+
+    
     ///INTRO GAME RULES
     cout << "Welcome to the Game ";
-    ///GET SECRET NUMBER
-    cout << "GIVE ME A NUMBER";
-    cout << "Between " << rangeBegining << " and " << rangeEnd << endl;
-    cin >> secretNumber;
-    //CHECK THAT IS INSIDE RANGE
     
     do{
+        ///GET SECRET NUMBER
+        cout << "GIVE ME A NUMBER";
+        cout << "Between " << rangeBegining << " and " << rangeEnd << endl;
+        cin >> secretNumber;
+        //CHECK THAT IS INSIDE RANGE
+        //HIDE FROM OTHER USER
         
-        cout << "PLAYER TWO, WHATS THE NUMBER?";
-        cout << endl << "Between " << rangeBegining << " and "
-             << rangeEnd << endl;
-        ///GUESS SECRET NUMBER
-        cin >> usersGuess;
-        //CHECK THAT IT IS INSIDE RANGE
-        
-        if(usersGuess==secretNumber){
-            guessCorrect=true;
-        }else{
-            guessCount++;
-            cout << guessCount;
-        }
+        do{
+            
+            cout << "PLAYER TWO, WHATS THE NUMBER?";
+            cout << endl << "Between " << rangeBegining << " and "
+                 << rangeEnd << endl;
+            ///GUESS SECRET NUMBER
+            cin >> usersGuess;
+            //CHECK THAT IT IS INSIDE RANGE
+            
+            
+            //CHECK GUESS
+            if(usersGuess==secretNumber){
+                guessCorrect=true;  //correct Guess
+            }else{
+                guessCount++;
+                
+                //OUTPUT GUESS HINTS
+                if(guessCount<MAX_GUESSES){
+                    if (usersGuess>secretNumber) {
+                        //cout
+                    }
+                }
+                
+            }
 
-    }while (!guessCorrect && guessCount<MAX_GUESSES);
-    
-    cout << "GAME OVER";
+        }while (!guessCorrect && guessCount<MAX_GUESSES);
+        
+        
+        //CHECK FOR WIN/LOSE
+        if(guessCorrect){
+            cout << "Congatulations, You Correctly Guessed the Secret Number,  YOU WIN!!";
+        }else{
+            cout << "Oh no,  you have run out of guesses without correctly guessing the secret number, YOU LOOSE!";
+            //cout << endl << "Do you want to know the secret number"
+        }
+        
+        //CHECK FOR PLAY AGAIN
+        cout << endl << "Play Again? (y/n):";
+        cin >> playAgain;
+       
+        
+    }while(playAgain.compare("n")!=0);
         
     return 0;
 }
+
