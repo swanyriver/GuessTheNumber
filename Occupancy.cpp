3751c123bf43d6e96efe277ff4a0238920d30873
Brandon Swanson <github@brandonswanson.com>
2014-06-27 16:26:16 -0700

    pluralization

--- Occupancy.cpp
+++ Occupancy.cpp
@@ -18,7 +18,7 @@
 using namespace std;
 
 int main(){
-	int numPeople, maxPeople, occupancyDifference
+	int numPeople, maxPeople, occupancyDifference;
     
     cout << "ITS PARTY TIME" << endl;
     cout << "How many people we got in here:";
@@ -28,13 +28,39 @@ int main(){
          << " how many people does the fire chief say we can have in here:";
     cin >> maxPeople;
     
-    if (numPeople<=maxPeople) {
-        cout << "PARTY ON!!!,  and qick call "
-        << maxPeople-numPeople << "more party people";
-    } else {
-        cout << "BAD PARTY! you have got to get "
-             << numPeople-maxPeople << " party people out of here now!";
+    occupancyDifference = maxPeople - numPeople;
+    
+    if (occupancyDifference>=0) {
+        switch (occupancyDifference) {
+            case 0:
+                cout << "PARTY ON!!!";
+                break;
+                
+            case 1:
+                cout << "PARTY ON!!! and qick call "
+                << " one more party person";
+                break;
+                
+            default:
+                cout << "PARTY ON!!!,  and qick call "
+                << occupancyDifference << " more party people";
+                break;
+        }
+    }else{ //occupanyDifferenc<0  too many people
+        switch (occupancyDifference) {
+            case -1:
+                cout << "BAD PARTY! you have got to get "
+                << " one party person out of here now!";
+                break;
+                
+            default:
+                cout << "BAD PARTY! you have got to get "
+                << occupancyDifference*-1 << " party people out of here now!";
+                break;
+        }
     }
+    
+    cout << endl;
     
     return 0;
 }
